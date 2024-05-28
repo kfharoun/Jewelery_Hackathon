c6b9e7488050ace9355e86522d11025f0411fa46
graceCancod3 <gracem@graces-iMac.local>
2024-05-28 16:33:47 -0400

    setting up controller boilerplate

--- Controllers/jewelryController.js
+++ Controllers/jewelryController.js
@@ -5,27 +5,23 @@ const mongoose = require('mongoose');
 const getAllJewelry = async (req, res) => {
 };
 
-const getAllProducts = async (req, res) => {
-};
-
 const getInfoById = async (req, res) => {
 };
 
-const deleteProduct = async (req, res) => {
+const deleteJewel = async (req, res) => {
 };
 
 
-const updateProduct = async (req, res) => {
+const updateJewel = async (req, res) => {
 };
 
-const createProduct = async (req, res) => {
+const createJewel = async (req, res) => {
 };
 
 module.exports = {
     getAllJewelry,
-    getAllProducts,
     getInfoById,
-    deleteProduct,
-    updateProduct,
-    createProduct
+    deleteJewel,
+    updateJewel,
+    createJewel
 };
