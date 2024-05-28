c3bb904eb03a4c2ea42af148cb53d60b1d701b86
graceCancod3 <gracem@graces-iMac.local>
2024-05-28 16:46:45 -0400

    commeting out controller functions

--- Controllers/jewelryController.js
+++ Controllers/jewelryController.js
@@ -2,26 +2,26 @@ const { Jewelry } = require('../models');
 const mongoose = require('mongoose');
 
 
-const getAllJewelry = async (req, res) => {
-};
+// const getAllJewelry = async (req, res) => {
+// };
 
-const getInfoById = async (req, res) => {
-};
+// const getInfoById = async (req, res) => {
+// };
 
-const deleteJewel = async (req, res) => {
-};
+// const deleteJewel = async (req, res) => {
+// };
 
 
-const updateJewel = async (req, res) => {
-};
+// const updateJewel = async (req, res) => {
+// };
 
-const createJewel = async (req, res) => {
-};
+// const createJewel = async (req, res) => {
+// };
 
 module.exports = {
-    getAllJewelry,
-    getInfoById,
-    deleteJewel,
-    updateJewel,
-    createJewel
+    // getAllJewelry,
+    // getInfoById,
+    // deleteJewel,
+    // updateJewel,
+    // createJewel
 };
--- Controllers/productController.js
+++ Controllers/productController.js
@@ -3,26 +3,26 @@ const mongoose = require('mongoose');
 
 
 
-const getAllProducts = async (req, res) => {
-};
+// const getAllProducts = async (req, res) => {
+// };
 
-const getInfoById = async (req, res) => {
-};
+// const getInfoById = async (req, res) => {
+// };
 
-const deleteProduct = async (req, res) => {
-};
+// const deleteProduct = async (req, res) => {
+// };
 
 
-const updateProduct = async (req, res) => {
-};
+// const updateProduct = async (req, res) => {
+// };
 
-const createProduct = async (req, res) => {
-};
+// const createProduct = async (req, res) => {
+// };
 
 module.exports = {
-    getAllProducts,
-    getInfoById,
-    deleteProduct,
-    updateProduct,
-    createProduct
+    // getAllProducts,
+    // getInfoById,
+    // deleteProduct,
+    // updateProduct,
+    // createProduct
 };
