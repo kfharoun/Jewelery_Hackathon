e71da9e7220339262b12185b1fcd6fe2ac9ee11c
graceCancod3 <gracem@graces-iMac.local>
2024-05-29 11:06:23 -0400

    added functionality to update, create, and delete jewel

--- Controllers/jewelryController.js
+++ Controllers/jewelryController.js
@@ -8,20 +8,49 @@ const mongoose = require('mongoose');
 // const getInfoById = async (req, res) => {
 // };
 
-// const deleteJewel = async (req, res) => {
-// };
+const deleteJewel = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const jewel = await Jewel.findByIdAndDelete(id);
+        if (!jewel) {
+            return res.status(404).json({ message: 'Jewel not found' });
+        }
+        res.status(200).json({ message: 'Jewel successfully deleted' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 
-// const updateJewel = async (req, res) => {
-// };
+const updateJewel = async (req, res) => {
+    const { id } = req.params;
+    const updateData = req.body;
+    try {
+        const jewel = await Product.findByIdAndUpdate(id, updateData, { new: true }).populate('jewelryType').populate('metalType');
+        if (!jewel) {
+            return res.status(404).json({ message: 'Jewel not found' });
+        }
+        res.status(200).json(jewel);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// const createJewel = async (req, res) => {
-// };
+const createJewel = async (req, res) => {
+    const { jewelryType, metalType } = req.body;
+    try {
+        const newJewel = new Jewel({ jewelryType, metalType });
+        await newJewel.save();
+        res.status(201).json(newJewel);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 module.exports = {
     // getAllJewelry,
     // getInfoById,
-    // deleteJewel,
-    // updateJewel,
-    // createJewel
+    deleteJewel,
+    updateJewel,
+    createJewel
 };
