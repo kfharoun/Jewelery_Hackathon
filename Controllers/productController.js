5db6b315b48164f98dd6a2def7e8ac9ee872a044
graceCancod3 <gracem@graces-iMac.local>
2024-05-29 09:39:54 -0400

    functionality added to product

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -3,26 +3,93 @@ const mongoose = require('mongoose');
 
 
 
-// const getAllProducts = async (req, res) => {
-// };
+const getAllJewelry = async (req, res) => {
+    try {
+        const jewelry = await jewl.find();
+        res.status(200).json(jewelry);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+    
+}
 
-// const getInfoById = async (req, res) => {
-// };
+const getAllProducts = async (req, res) => {
+    const { sortBy } = req.query;
+    let sortCriteria = {};
 
-// const deleteProduct = async (req, res) => {
-// };
+    if (sortBy && (sortBy === 'priceAsc' || sortBy === 'priceDesc')) {
+        sortCriteria = sortBy === 'priceAsc' ? { price: 1 } : { price: -1 };
+    }
+
+    try {
+        const products = await Product.find()
+            .populate('name')
+            .populate('description')
+            .populate('image')
+            .sort(sortCriteria);
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getInfoById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id).populate('name').populate('description').populate('image');
+        if (!product) {
+            return res.status(404).json({ message: 'Jewelry not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json({ message: 'Product successfully deleted' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 
-// const updateProduct = async (req, res) => {
-// };
+const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const updateData = req.body;
+    try {
+        const product = await Product.findByIdAndUpdate(id, updateData, { new: true }).populate('name').populate('jewelryId');
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// const createProduct = async (req, res) => {
-// };
+const createProduct = async (req, res) => {
+    const { name, price, inStock, description, bestSeller, image, jewelryId } = req.body;
+    try {
+        const newProduct = new Product({ name, price, inStock, description, bestSeller, image, jewelryId });
+        await newProduct.save();
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 module.exports = {
-    // getAllProducts,
-    // getInfoById,
-    // deleteProduct,
-    // updateProduct,
-    // createProduct
+    getAllJewelry,
+    getAllProducts,
+    getInfoById,
+    deleteProduct,
+    updateProduct,
+    createProduct
 };
